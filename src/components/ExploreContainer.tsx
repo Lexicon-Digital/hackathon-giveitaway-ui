78efc13faf6164217f67c5356d6a60214e0a5150
johnver <john.verdeguzman@ioof.com.au>
2021-12-21 11:36:57 +1100

    Display map view

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -1,14 +1,19 @@
 import './ExploreContainer.css';
+import MapComponent from './MapComponent';
 
 interface ContainerProps {
   name: string;
 }
 
 const ExploreContainer: React.FC<ContainerProps> = ({ name }) => {
+
+  const center = { lat: -34.397, lng: 150.644 };
+  const zoom = 4;
   return (
     <div className="container">
       <strong>{name}</strong>
       <p>Explore <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+        <MapComponent center={center} zoom={zoom} />
     </div>
   );
 };
